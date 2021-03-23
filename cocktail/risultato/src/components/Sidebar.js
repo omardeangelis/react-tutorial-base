6fd90af357748800b034ab8c773e753c078b076a
Omar De Angelis <omar.hpvfilm@gmail.com>
2021-03-23 17:46:54 +0100

    css cross-browser optimization

--- cocktail/risultato/src/components/Sidebar.js
+++ cocktail/risultato/src/components/Sidebar.js
@@ -6,18 +6,20 @@ const Sidebar = () => {
   const { closeSidebar, isSidebarOpen } = useGlobalContext();
   return (
     <aside className={`${isSidebarOpen ? "sidebar show-sidebar" : "sidebar"}`}>
-      <header className="nav-header container">
-        <div className="nav-brand">
-          <h3>DRINK TEAM</h3>
+      <div className="sidebar-content">
+        <header className="nav-header container">
+          <div className="nav-brand">
+            <h3>DRINK TEAM</h3>
+          </div>
+          <button className="nav-toggler btn icon-btn" onClick={closeSidebar}>
+            <VscArrowSmallLeft className="nav-icon" />
+          </button>
+        </header>
+        <div className="container">
+          <LinkComponent classLink="sidebar-links" />
         </div>
-        <button className="nav-toggler btn icon-btn" onClick={closeSidebar}>
-          <VscArrowSmallLeft className="nav-icon" />
-        </button>
-      </header>
-      <div className="container">
-        <LinkComponent classLink="sidebar-links" />
+        <SocialComponent classSocial="sidebar-social" />
       </div>
-      <SocialComponent classSocial="sidebar-social" />
     </aside>
   );
 };
