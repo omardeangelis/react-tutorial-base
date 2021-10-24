87f22580c972c8918e03def109a47b8edf6cd046
Omar De Angelis <omar.hpvfilm@gmail.com>
2021-10-24 20:19:57 +0200

    feat(component): Create Input styled-component

---
+++ pick-me/risultato/src/components/styled/Input.js
@@ -0,0 +1,36 @@
+import styled from "styled-components";
+import {
+  layout,
+  space,
+  border,
+  typography,
+  shadow,
+  color,
+  background,
+  compose,
+} from "styled-system";
+
+const InputWrapper = styled("input")(
+  {
+    background: "var(--grey-900)",
+    border: " 1px solid var(--grey-700)",
+    color: "var(--grey-700)",
+    height: "48px",
+    cursor: "text",
+    borderRadius: "100px",
+    paddingLeft: "20px",
+    paddingRight: "20px",
+    appearance: "none",
+    outline: "none",
+    fontSize: "16px",
+    lineHeight: "20px",
+    "&:placeholder": {
+      color: "var(--grey-600)",
+      lineHeight: "20px",
+    },
+  },
+
+  compose(layout, space, border, typography, shadow, color, background)
+);
+
+export default InputWrapper;
