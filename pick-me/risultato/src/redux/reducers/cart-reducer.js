a4f6e2d1ee8c516311b6a0e7c030c418023afe43
Omar De Angelis <omar.hpvfilm@gmail.com>
2021-11-02 23:44:00 +0100

    feat: basic reducer & extra reducer implementation

--- pick-me/risultato/src/redux/reducers/cart-reducer.js
+++ pick-me/risultato/src/redux/reducers/cart-reducer.js
@@ -1,25 +1,57 @@
-import { createSlice } from "@reduxjs/toolkit";
+import { createSlice, createAction } from "@reduxjs/toolkit";
+
+const isAddToCartAction = (action) => {
+  return action.type.endsWith("/addToCart");
+};
+
+const removeFromCart = createAction("remove-from-cart");
+const calcualteTotal = createAction("find-total");
 
 const initialValue = {
-  value: 0,
+  cart: [],
+  total: 0,
+  payed: false,
 };
 
 export const cartSlice = createSlice({
   name: "cart",
   initialState: initialValue,
   reducers: {
-    increment: (state) => {
-      state.value += 1;
+    cleanCart: (state) => {
+      state.cart = [];
+      state.total = 0;
     },
-    decrement: (state) => {
-      state.value -= 1;
+    addToCart: (state, action) => {
+      state.cart.push(action.payload);
     },
-    incrementByAmount: (state, { type, payload }) => {
-      state.value += payload;
-    },
+  },
+  extraReducers: (builder) => {
+    builder
+      .addCase(removeFromCart, (state, action) => {
+        state.cart = state.cart.filter((el) => el.id !== action.payload);
+      })
+      .addCase(calcualteTotal, (state, action) => {
+        state.total = action.payload;
+      })
+      .addMatcher(isAddToCartAction, (state, action) => {
+        state.total += action.payload.likes;
+      })
+      .addDefaultCase((state) => {
+        return state;
+      });
   },
 });
 
-export const { increment, incrementByAmount, decrement } = cartSlice.actions;
+export const { cleanCart, addToCart } = cartSlice.actions;
+
+export { removeFromCart };
+
+export const findCartTotal = () => (dispatch, getState) => {
+  let cartPrice = getState().cart.cart.reduce((total, item) => {
+    return total + item.likes;
+  }, 0);
+
+  dispatch({ type: calcualteTotal, payload: cartPrice });
+};
 
 export default cartSlice.reducer;
